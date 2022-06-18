44b251e57ce9b72fda556a202361824e6e369fb2
DanielHP <danielh.possamai@gmail.com>
2022-06-18 11:01:10 -0300

    feito tarefa 3

--- primeiro-arquivo-java/src/br/com/aula/atividades/Atividade1.java
+++ primeiro-arquivo-java/src/br/com/aula/atividades/Atividade1.java
@@ -3,27 +3,25 @@ package br.com.aula.atividades;
 import java.util.Scanner;
 
 public class Atividade1 {
-	
-	//colete o nome do usuário
-	//colete a idade de usuário
-	//faça o sistema dizer "<nome_usuario>, menores de idade nao são permitidos"
-	//faça o sistema dizer "<nome_usuario> Seja Bem Vindo!"
-	
+
+	// colete o nome do usuário
+	// colete a idade de usuário
+	// faça o sistema dizer "<nome_usuario>, menores de idade nao são permitidos"
+	// faça o sistema dizer "<nome_usuario> Seja Bem Vindo!"
+
 	public static void main(String[] args) {
-		
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Digite seu nome:");
 		String nome = sc.next();
-		
+
 		System.out.println("Digite sua idade:");
 		int age = sc.nextInt();
-		
-		
-		if(age < 18) {
+
+		if (age < 18) {
 			System.out.println(nome + ", menores de idade nao sao permitidos");
-		} else { 
+		} else {
 			System.out.println(nome + ". Seja bem Vindo!");
-	}
+		}
 	}
 }
